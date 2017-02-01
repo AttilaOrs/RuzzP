86c30cb83314936d2953e48de196671fc4f7b3bb
Kacsa Joco <sohanemtevedek@gmail.com>
2017-02-01 10:11:30 +0100

    sceletion for table parsing added

--- src/petri_net/mod.rs
+++ src/petri_net/mod.rs
@@ -2,7 +2,7 @@ mod petri_net_builder;
 mod petri_dot_builder;
 mod petri_executor;
 
-pub use self::petri_net_builder::{FuzzyPetriNetBuilder, FuzzyPetriNet};
+pub use self::petri_net_builder::{FuzzyPetriNetBuilder, FuzzyPetriNet, FuzzyTableE};
 pub use self::petri_net_builder::{EventManager,FuzzyTokenConsumer};
 pub use self::petri_executor::SynchronousFuzzyPetriExecutor;
 pub use self::petri_dot_builder::DotStringBuilder;
--- src/read_petri/petri_json_reader.rs
+++ src/read_petri/petri_json_reader.rs
@@ -7,6 +7,7 @@ use std::collections::BTreeMap;
 
 use basic::*;
 use tables::*;
+use petri_net::{FuzzyPetriNetBuilder,FuzzyTableE};
 
 use self::NotExpectedJsonFormat::*;
 
@@ -48,13 +49,16 @@ fn my_file_read(fname: &str) -> String {
 static TR_NR: &'static str = "transitionCntr";
 static PL_NR: &'static str = "placeCntr";
 static INP_PL: &'static str = "isInputPlaces";
+static OUT_TR: &'static str = "isOutputTransition";
 static INIT_PL: &'static str = "initialMarkingOfThePlaces";
 static PHI: &'static str = "phi";
 static FV_S: &'static str = "fuzzyValues";
 static TR_TO_PL: &'static str = "fromTransToPlace";
 static PL_TO_PL: &'static str = "fromPlaceToTrans";
 static WEIGTHS: &'static str = "weights";
-
+static TBL_TRS: &'static str = "tableForTransition";
+static TBL_TYPE: &'static str = "type";
+static TBL_DATA: &'static str = "data";
 macro_rules! mine {
     ($obj:ident, $fnc:ident, $idd: ident ) => {
          $obj.get($idd).ok_or(JsonKeyNotFound($idd))?
@@ -62,6 +66,11 @@ macro_rules! mine {
     }
 }
 
+macro_rules! assert_length {
+    ($vecc: ident, $len: ident, $err_hint: ident ) => {
+            if $vecc.len() != $len {return Err(WrongNumberOfStuff($err_hint))};
+    }
+}
 
 
 fn deseralize(what :&str) -> Result<i32> {
@@ -72,28 +81,74 @@ fn deseralize(what :&str) -> Result<i32> {
      let pl_nr = mine!(obj, as_u64, PL_NR) as usize;
 
      let inp_pl_jsons = mine!(obj, as_array, INP_PL);
-     let inp_pl = mine_is_inp_place(inp_pl_jsons)?;
+     let inp_pl = mine_bool_vec(inp_pl_jsons, INP_PL)?;
+     assert_length!(inp_pl, pl_nr, INP_PL);
+
+     let out_tr_jsons = mine!(obj, as_array, OUT_TR);
+     let out_tr = mine_bool_vec(out_tr_jsons, OUT_TR)?;
+     assert_length!(out_tr, tr_nr, OUT_TR);
 
      let inital_marking_jsons = mine!(obj, as_array, INIT_PL);
      let init_marking  = mine_init_marking(inital_marking_jsons)?;
-     if init_marking.len() != pl_nr {return Err(WrongNumberOfStuff(INIT_PL))};
+     assert_length!(init_marking, pl_nr, INIT_PL);
 
      let tr_to_pl_jsons  = mine!(obj, as_array, TR_TO_PL);
-     let to_to_pl = mine_arcs(tr_to_pl_jsons, TR_TO_PL)?;
-     if to_to_pl.len() != tr_nr {return Err(WrongNumberOfStuff(TR_TO_PL))};
+     let tr_to_pl = mine_arcs(tr_to_pl_jsons, TR_TO_PL)?;
+     assert_length!(tr_to_pl, tr_nr, TR_TO_PL);
 
      let pl_to_tr_jsons  = mine!(obj, as_array, PL_TO_PL);
      let pl_to_tr = mine_arcs(pl_to_tr_jsons, PL_TO_PL)?;
-     if pl_to_tr.len() != pl_nr {return Err(WrongNumberOfStuff(PL_TO_PL))};
+     assert_length!(pl_to_tr, pl_nr, PL_TO_PL);
 
      let weigth_jsons  = mine!(obj, as_object, WEIGTHS);
      let weigths = mine_weigth(weigth_jsons)?;
+     assert_length!(weigths, pl_nr, WEIGTHS);
+
+     let table_jsons  = mine!(obj, as_array, TBL_TRS);
+     let tables = mine_tables(table_jsons);
 
      println!("{:?}", weigths);
 
 
      Ok(12)
 }
+
+fn mine_tables(table_jsons: &Vec<Json>) ->Result<Vec<FuzzyTableE>> {
+    let mut to_ret = Vec::new();
+    for table_json in table_jsons {
+        let table_obj = table_json.as_object().ok_or(WrongJsonValue(TBL_TRS))?;
+        let table_type = table_obj
+            .get(TBL_TYPE).ok_or(WrongJsonValue(TBL_TYPE))?
+            .as_string().ok_or(WrongJsonValue(TBL_TYPE))?;
+        let table_data_obj = table_obj
+            .get(TBL_DATA).ok_or(WrongJsonValue(TBL_DATA))?
+            .as_object().ok_or(WrongJsonValue(TBL_DATA))?;
+        let table = match table_type.as_ref() {
+            "1x1" => mine_oxo(table_data_obj),
+            "2x1" => mine_txo(table_data_obj),
+            "1x2" => mine_oxt(table_data_obj),
+            "2x2" => mine_txt(table_data_obj),
+            _     => {return Err(WrongJsonValue(TBL_TYPE))}
+        };
+        to_ret.push(table);
+    }
+    Ok(to_ret)
+}
+ fn mine_oxo(data : &BTreeMap<String, Json> ) -> FuzzyTableE {
+     FuzzyTableE::oxo(OneXOneTable::default_table())
+ }
+
+ fn mine_txo(data : &BTreeMap<String, Json> ) -> FuzzyTableE {
+     FuzzyTableE::txo(TwoXOneTable::default_table())
+ }
+
+ fn mine_oxt(data : &BTreeMap<String, Json> ) -> FuzzyTableE {
+     FuzzyTableE::oxt(OneXTwoTable::default_table())
+ }
+
+ fn mine_txt(data : &BTreeMap<String, Json> ) -> FuzzyTableE {
+     FuzzyTableE::txt(TwoXTwoTable::default_table())
+ }
 
 fn mine_weigth(weigth: &BTreeMap<String, Json>) -> Result<Vec<(usize, usize, f32)>> {
     let mut to_ret = Vec::new();
@@ -113,7 +168,7 @@ fn mine_weigth(weigth: &BTreeMap<String, Json>) -> Result<Vec<(usize, usize, f32
     Ok(to_ret)
 }
 
-fn mine_arcs( arc_jsons: &Vec<Json>,  talking_about :&'static str ) -> Result<Vec<Vec<usize>>> {
+fn mine_arcs( arc_jsons: &Vec<Json>,  talking_about: &'static str ) -> Result<Vec<Vec<usize>>> {
     let mut to_ret = Vec::new();
     for inner_json in arc_jsons {
         let unmined = inner_json.as_array()
@@ -130,11 +185,11 @@ fn mine_arcs( arc_jsons: &Vec<Json>,  talking_about :&'static str ) -> Result<Ve
     Ok(to_ret)
 }
 
-fn mine_is_inp_place(bools: &Vec<Json>) -> Result<Vec<bool>> {
+fn mine_bool_vec(bools: &Vec<Json>, talking_about: &'static str) -> Result<Vec<bool>> {
     let mut to_ret = Vec::new();
     for v in bools {
         let unmined = v.as_boolean()
-            .ok_or(WrongJsonValue(INP_PL))?;
+            .ok_or(WrongJsonValue(talking_about))?;
         to_ret.push(unmined);
     }
     Ok(to_ret)
@@ -170,9 +225,9 @@ mod tests {
 
     #[test]
     fn ww() {
-        let ww = super::my_file_read("inputs/SimpleDelayPetriNet.json");
+        let ww = super::my_file_read("inputs/TwoLoopPetriNet.json");
         let rez = super::deseralize(&ww);
         println!("{:?}", rez);
-        assert!(true);
+        assert!(false);
     }
 }
